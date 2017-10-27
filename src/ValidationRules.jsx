5463de3a9ce721485dbf9cff9ca5596cb15bcda7
Kadir Özdemir <ko@webberz.at>
2017-10-27 16:39:01 +0200

    travis fix

--- src/ValidationRules.jsx
+++ src/ValidationRules.jsx
@@ -43,9 +43,9 @@ const validations = {
 
     minNumber: (value, min) => !isExisty(value) || isEmpty(value) || parseInt(value, 10) >= parseInt(min, 10),
 
-    isString: (value)  =>  !isEmpty(value) || typeof value === 'string' || value instanceof String,
+    isString: value => !isEmpty(value) || typeof value === 'string' || value instanceof String,
     minStringLength: (value, length) => validations.isString(value) || value.length >= length,
-    maxStringLength: (value, length) => validations.isString(value) || value.length <= length
+    maxStringLength: (value, length) => validations.isString(value) || value.length <= length,
 };
 
 module.exports = validations;
