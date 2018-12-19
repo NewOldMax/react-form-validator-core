226887c26518bb69bfd8e1521eaf0a3d7efdaff0
kizaonline <kizaonline@users.noreply.github.com>
2018-12-19 22:40:24 +1100

    Update ValidationRules.jsx

--- src/ValidationRules.jsx
+++ src/ValidationRules.jsx
@@ -33,7 +33,7 @@ const validations = {
 
     isNumber: value => validations.matchRegexp(value, /^-?[0-9]\d*(\d+)?$/i),
 
-    isFloat: value => validations.matchRegexp(value, /^(?:-?[1-9]\d*|-?0)?(?:\.\d+)?$/i),
+    isFloat: value => validations.matchRegexp(value, /^(?:-?[1-9]\d*|0)?(?:\.\d+)?$/i),
 
     isPositive: (value) => {
         if (isExisty(value)) {
