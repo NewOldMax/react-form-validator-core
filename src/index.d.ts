753560d16cef17a7f76cb3344ec936a8aa88cc59
Siraj Alam <sirajalam049@gmail.com>
2019-07-04 19:47:19 +0530

    Added typings for index.js

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+
+export { default as ValidatorComponent } from './ValidatorComponent';
+
+export * from './ValidatorComponent'
+
+export { default as ValidatorForm } from './ValidatorForm';
+
+export * from './ValidatorForm';
+
+export { default as ValidationRules } from './ValidationRules';
+
+export * from './ValidationRules';
